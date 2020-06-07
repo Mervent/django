43764bc8c8961d87c10426ffa87b7342029d307f
mervent <22627512+Mervent@users.noreply.github.com>
2020-06-07 13:37:55 +0300

    reorganize installed apps

--- {{cookiecutter.project_name}}/src/app/settings.py
+++ {{cookiecutter.project_name}}/src/app/settings.py
@@ -23,17 +23,17 @@ DEBUG = env.bool("DEBUG", default=False)
 ALLOWED_HOSTS = ["*"]  # disable weird host validation
 
 INSTALLED_APPS = [
-    "app",
-    "users",
-    "rest_framework",
-    "rest_framework.authtoken",
-    "rest_framework_jwt.blacklist",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework.authtoken",
+    "rest_framework_jwt.blacklist",
+    "app",
+    "users",
 ]
 
 MIDDLEWARE = [
